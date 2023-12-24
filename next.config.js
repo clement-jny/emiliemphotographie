3693481869c29d384f90e103f63d41d49376a6bb
clement-jny <82049263+clement-jny@users.noreply.github.com>
2023-12-24 03:41:35 +0100

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-// const githubRepo = 'emiliemphotographie'
-// const assetPrefix = `/${githubRepo}/`
-// const basePath = `/${githubRepo}`
+const githubRepo = 'emiliemphotographie'
+const assetPrefix = `/${githubRepo}/`
+const basePath = `/${githubRepo}`
 
 const nextConfig = {
   // Export settings for static site generation -> github pages
   output: 'export',
-  // assetPrefix: assetPrefix,
-  // basePath: basePath
+  assetPrefix: assetPrefix,
+  basePath: basePath
 }
 
 module.exports = nextConfig
