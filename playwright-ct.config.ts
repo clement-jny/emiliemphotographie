15f005f682b8cc2e92b5efacc2b4e765a367df8e
clement-jny <82049263+clement-jny@users.noreply.github.com>
2024-01-17 17:11:19 +0100

    feat: update playwright's config files

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -6,20 +6,31 @@ import { resolve } from 'path';
  */
 export default defineConfig({
   testDir: './playwright/component',
+
   /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
   snapshotDir: './__snapshots__',
+
+  // Folder for test artifacts such as screenshots, videos, traces, etc.
+  outputDir: './playwright/.results/component',
+
   /* Maximum time one test can run for. */
   timeout: 10 * 1000,
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never', outputFolder: './playwright/.reports/component' }]],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
--- playwright.config.ts
+++ playwright.config.ts
@@ -11,16 +11,28 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './playwright/e2e',
+
+  // Folder for test artifacts such as screenshots, videos, traces, etc.
+  outputDir: './playwright/.results/e2e',
+
+  /* Maximum time one test can run for. */
+  timeout: 10 * 1000,
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never', outputFolder: './playwright/.reports/e2e' }]],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
