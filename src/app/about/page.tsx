384c10e5c7a52f2df54b6b9e91ef2912dfd996ef
clement-jny <82049263+clement-jny@users.noreply.github.com>
2024-08-10 22:34:41 +0200

    feat: create about page; chore: remove css file

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { SharedLayout } from '@/components/shared-layout';
+
+const AboutPage = () => {
+  return (
+    <SharedLayout>
+      <h1>About Page</h1>
+    </SharedLayout>
+  );
+};
+
+export default AboutPage;
